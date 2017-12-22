138ea71bacc6a6a83f950312c33b84e375aa2cf5
sasithorn2 <33056752+sasithorn2@users.noreply.github.com>
2017-12-22 14:13:58 +0700

    7.1

--- lab_7_1.cpp
+++ lab_7_1.cpp
@@ -9,19 +9,11 @@ int main() {
   int i = 0;
 
   // ศึกษาตัวอย่างการใช้ for
-  for(i = 0; i < 10 ; i++) {
-    cout << "Round " << i << endl;
+  for(i = 0; i < 8 ; i++) {
+    cout << "Hello World " << i << endl;
   }
 
   // TODO : แก้ไขส่วนนี้ด้วยการใช้ for
-  cout << "Hello World 1" << endl;
-  cout << "Hello World 2" << endl;
-  cout << "Hello World 3" << endl;
-  cout << "Hello World 4" << endl;
-  cout << "Hello World 5" << endl;
-  cout << "Hello World 6" << endl;
-  cout << "Hello World 7" << endl;
-  cout << "Hello World 8" << endl;
 
   return 0;
 }
